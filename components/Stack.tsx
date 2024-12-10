213b263f544332e2cbda2bdceaff2bb584b77204
Trend20 <enockomondi305@gmail.com>
2024-12-10 21:04:22 +0300

    tech stack UI modifications

--- components/Stack.tsx
+++ components/Stack.tsx
@@ -1,37 +1,38 @@
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
-import {ToolsGrid} from "@/components/ToolsGrid";
-import {backendTools, cloudTools, deploymentTools, frontendTools} from "@/data";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { ToolsGrid } from "@/components/ToolsGrid";
+import { Databases, Frameworks, Languages, Tools } from "@/data";
 
 export default function Stack() {
-    return (
-        <div className="min-h-screen bg-teal-900 text-white py-16 px-4">
-            <div className="container mx-auto">
-                <div className="text-center mb-12">
-                    <h3 className="text-teal-400 mb-4">Crafting Excellence with</h3>
-                    <h2 className="text-4xl font-bold">Modern Tools</h2>
-                </div>
-                <Tabs defaultValue="frontend" className="w-full">
-                    <TabsList className="w-full max-w-2xl mx-auto mb-12 bg-teal-800">
-                        <TabsTrigger value="frontend" className="flex-1 text-lg">Frontend</TabsTrigger>
-                        <TabsTrigger value="backend" className="flex-1 text-lg">Backend</TabsTrigger>
-                        <TabsTrigger value="cloud" className="flex-1 text-lg">Cloud</TabsTrigger>
-                        <TabsTrigger value="deployment" className="flex-1 text-lg">Deployment</TabsTrigger>
-                    </TabsList>
-                    <TabsContent value="frontend">
-                        <ToolsGrid tools={frontendTools} />
-                    </TabsContent>
-                    <TabsContent value="backend">
-                        <ToolsGrid tools={backendTools} />
-                    </TabsContent>
-                    <TabsContent value="cloud">
-                        <ToolsGrid tools={cloudTools} />
-                    </TabsContent>
-                    <TabsContent value="deployment">
-                        <ToolsGrid tools={deploymentTools} />
-                    </TabsContent>
-                </Tabs>
-            </div>
-        </div>
-    )
+  return (
+    <div className="min-h-screen py-5">
+      <Tabs defaultValue="languages" className="w-full">
+        <TabsList className="w-full max-w-4xl mx-auto mb-12 py-4">
+          <TabsTrigger value="languages" className="flex-1 text-lg">
+            Programming Languages
+          </TabsTrigger>
+          <TabsTrigger value="frameworks" className="flex-1 text-lg">
+            Frameworks & Libraries
+          </TabsTrigger>
+          <TabsTrigger value="databases" className="flex-1 text-lg">
+            Databases
+          </TabsTrigger>
+          <TabsTrigger value="tools" className="flex-1 text-lg">
+            Tools
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value="languages">
+          <ToolsGrid tools={Languages} />
+        </TabsContent>
+        <TabsContent value="frameworks">
+          <ToolsGrid tools={Frameworks} />
+        </TabsContent>
+        <TabsContent value="databases">
+          <ToolsGrid tools={Databases} />
+        </TabsContent>
+        <TabsContent value="tools">
+          <ToolsGrid tools={Tools} />
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
 }
-
--- data/index.ts
+++ data/index.ts
@@ -1,289 +1,281 @@
-import {BlogPost, ISkillListItem, SkillLevel} from "@/types";
+import { BlogPost, ISkillListItem, SkillLevel } from "@/types";
 
 export const skills: ISkillListItem[] = [
-    {
-        title: "Programming Languages",
-        items: [
-            {
-                title: "JavaScript",
-                level: SkillLevel.Expert,
-                icon: "/skills/javascript.svg",
-            },
-            {
-                title: "TypeScript",
-                level: SkillLevel.Expert,
-                icon: "/skills/typescript.svg",
-            },
-            {
-                title: "Golang",
-                level: SkillLevel.Expert,
-                icon: "/skills/go.svg",
-            },
-        ],
-    },
-    {
-        title: "Frontend Development",
-        items: [
-            {
-                title: "Next.js",
-                level: SkillLevel.Expert,
-                icon: "/skills/nextjs.png",
-            },
-            {
-                title: "React.js",
-                level: SkillLevel.Expert,
-                icon: "/skills/react.svg",
-            },
-            {
-                title: "HTML",
-                level: SkillLevel.Expert,
-                icon: "/skills/html.svg",
-            },
-            {
-                title: "CSS",
-                level: SkillLevel.Expert,
-                icon: "/skills/css.svg",
-            },
-            {
-                title: "SASS",
-                level: SkillLevel.Intermediate,
-                icon: "/skills/sass.svg",
-            },
-            {
-                title: "TailwindCSS",
-                level: SkillLevel.Intermediate,
-                icon: "/skills/tailwindcss.svg",
-            },
-            {
-                title: "Redux Toolkit",
-                level: SkillLevel.Expert,
-                icon: "/skills/redux.svg",
-            },
-        ],
-    },
-    {
-        title: "Backend Development",
-        items: [
-            {
-                title: "Node.js",
-                level: SkillLevel.Expert,
-                icon: "/skills/nodejs.svg",
-            },
-            {
-                title: "Express.js",
-                level: SkillLevel.Expert,
-                icon: "/skills/express.svg",
-            },
-            {
-                title: "Golang",
-                level: SkillLevel.Expert,
-                icon: "/skills/go.svg",
-            },
-            {
-                title: "Nest.js",
-                level: SkillLevel.Intermediate,
-                icon: "/skills/nestjs.svg",
-            },
-        ],
-    },
-    {
-        title: "Mobile App Development",
-        items: [
-            {
-                title: "React Native",
-                level: SkillLevel.Intermediate,
-                icon: "/skills/native.svg",
-            },
-        ],
-    },
-    {
-        title: "Database Management",
-        items: [
-            {
-                title: "MongoDB",
-                level: SkillLevel.Intermediate,
-                icon: "/skills/mongodb.svg",
-            },
-            {
-                title: "PostgreSQL",
-                level: SkillLevel.Intermediate,
-                icon: "/skills/postgresql.svg",
-            },
-            {
-                title: "MySQL",
-                level: SkillLevel.Begginer,
-                icon: "/skills/mysql.svg",
-            },
-        ],
-    },
-    {
-        title: "DevOps/VCS",
-        items: [
-            {
-                title: "Docker",
-                level: SkillLevel.Begginer,
-                icon: "/skills/docker.png",
-            },
-            {
-                title: "AWS",
-                level: SkillLevel.Intermediate,
-                icon: "/skills/aws.svg",
-            },
-            {
-                title: "Git",
-                level: SkillLevel.Expert,
-                icon: "/skills/git.svg",
-            },
-            {
-                title: "GitHub",
-                level: SkillLevel.Expert,
-                icon: "/skills/github.svg",
-            },
-        ],
-    },
+  {
+    title: "Programming Languages",
+    items: [
+      {
+        title: "JavaScript",
+        level: SkillLevel.Expert,
+        icon: "/skills/javascript.svg",
+      },
+      {
+        title: "TypeScript",
+        level: SkillLevel.Expert,
+        icon: "/skills/typescript.svg",
+      },
+      {
+        title: "Golang",
+        level: SkillLevel.Expert,
+        icon: "/skills/go.svg",
+      },
+    ],
+  },
+  {
+    title: "Frontend Development",
+    items: [
+      {
+        title: "Next.js",
+        level: SkillLevel.Expert,
+        icon: "/skills/nextjs.png",
+      },
+      {
+        title: "React.js",
+        level: SkillLevel.Expert,
+        icon: "/skills/react.svg",
+      },
+      {
+        title: "HTML",
+        level: SkillLevel.Expert,
+        icon: "/skills/html.svg",
+      },
+      {
+        title: "CSS",
+        level: SkillLevel.Expert,
+        icon: "/skills/css.svg",
+      },
+      {
+        title: "SASS",
+        level: SkillLevel.Intermediate,
+        icon: "/skills/sass.svg",
+      },
+      {
+        title: "TailwindCSS",
+        level: SkillLevel.Intermediate,
+        icon: "/skills/tailwindcss.svg",
+      },
+      {
+        title: "Redux Toolkit",
+        level: SkillLevel.Expert,
+        icon: "/skills/redux.svg",
+      },
+    ],
+  },
+  {
+    title: "Backend Development",
+    items: [
+      {
+        title: "Node.js",
+        level: SkillLevel.Expert,
+        icon: "/skills/nodejs.svg",
+      },
+      {
+        title: "Express.js",
+        level: SkillLevel.Expert,
+        icon: "/skills/express.svg",
+      },
+      {
+        title: "Golang",
+        level: SkillLevel.Expert,
+        icon: "/skills/go.svg",
+      },
+      {
+        title: "Nest.js",
+        level: SkillLevel.Intermediate,
+        icon: "/skills/nestjs.svg",
+      },
+    ],
+  },
+  {
+    title: "Mobile App Development",
+    items: [
+      {
+        title: "React Native",
+        level: SkillLevel.Intermediate,
+        icon: "/skills/native.svg",
+      },
+    ],
+  },
+  {
+    title: "Database Management",
+    items: [
+      {
+        title: "MongoDB",
+        level: SkillLevel.Intermediate,
+        icon: "/skills/mongodb.svg",
+      },
+      {
+        title: "PostgreSQL",
+        level: SkillLevel.Intermediate,
+        icon: "/skills/postgresql.svg",
+      },
+      {
+        title: "MySQL",
+        level: SkillLevel.Begginer,
+        icon: "/skills/mysql.svg",
+      },
+    ],
+  },
+  {
+    title: "DevOps/VCS",
+    items: [
+      {
+        title: "Docker",
+        level: SkillLevel.Begginer,
+        icon: "/skills/docker.png",
+      },
+      {
+        title: "AWS",
+        level: SkillLevel.Intermediate,
+        icon: "/skills/aws.svg",
+      },
+      {
+        title: "Git",
+        level: SkillLevel.Expert,
+        icon: "/skills/git.svg",
+      },
+      {
+        title: "GitHub",
+        level: SkillLevel.Expert,
+        icon: "/skills/github.svg",
+      },
+    ],
+  },
 ];
-
 
 // projects
 
 export const projects = [
-    {
-        title: "Homemade GPT • JS",
-        icon: "💬",
-        date: "Nov 2024",
-        stars: 61,
-        description: "A minimal TensorFlow.js re-implementation of Karpathy's minGPT (Generative Pre-trained Transformer)",
-        tags: ["GPT", "Transformers", "TensorFlow"],
-        image: "/images/automate.png",
-    },
-    {
-        title: "Micrograd TS",
-        icon: "🤖",
-        date: "Aug 2023",
-        stars: 67,
-        description: "A tiny scalar-valued autograd engine and a neural net on top of it. A TypeScript version of the karpathy/micrograd repo.",
-        tags: ["AI", "ML", "Machine Learning"],
-        image: "/images/automate.png",
-    },
-    {
-        title: "Observations",
-        icon: "👁️",
-        date: "Jun 2023",
-        stars: 0,
-        description: "Subjective graph of observations, assumptions, questions, and interpretations",
-        tags: ["Observations", "Thoughts"],
-        image: "/images/automate.png",
-        hasSource: false,
-    },
-    {
-        title: "AutoMate",
-        icon: "👁️",
-        date: "Jun 2023",
-        stars: 0,
-        description: "Subjective graph of observations, assumptions, questions, and interpretations",
-        tags: ["Observations", "Thoughts"],
-        image: "/images/automate.png",
-        hasSource: false,
-    },
-    {
-        title: "Apitestdoc",
-        icon: "👁️",
-        date: "Jun 2023",
-        stars: 0,
-        description: "Subjective graph of observations, assumptions, questions, and interpretations",
-        tags: ["Observations", "Thoughts"],
-        image: "/images/apitest.png",
-        hasSource: false,
-    },
-]
+  {
+    title: "Homemade GPT • JS",
+    icon: "💬",
+    date: "Nov 2024",
+    stars: 61,
+    description:
+      "A minimal TensorFlow.js re-implementation of Karpathy's minGPT (Generative Pre-trained Transformer)",
+    tags: ["GPT", "Transformers", "TensorFlow"],
+    image: "/images/automate.png",
+  },
+  {
+    title: "Micrograd TS",
+    icon: "🤖",
+    date: "Aug 2023",
+    stars: 67,
+    description:
+      "A tiny scalar-valued autograd engine and a neural net on top of it. A TypeScript version of the karpathy/micrograd repo.",
+    tags: ["AI", "ML", "Machine Learning"],
+    image: "/images/automate.png",
+  },
+  {
+    title: "Observations",
+    icon: "👁️",
+    date: "Jun 2023",
+    stars: 0,
+    description:
+      "Subjective graph of observations, assumptions, questions, and interpretations",
+    tags: ["Observations", "Thoughts"],
+    image: "/images/automate.png",
+    hasSource: false,
+  },
+  {
+    title: "AutoMate",
+    icon: "👁️",
+    date: "Jun 2023",
+    stars: 0,
+    description:
+      "Subjective graph of observations, assumptions, questions, and interpretations",
+    tags: ["Observations", "Thoughts"],
+    image: "/images/automate.png",
+    hasSource: false,
+  },
+  {
+    title: "Apitestdoc",
+    icon: "👁️",
+    date: "Jun 2023",
+    stars: 0,
+    description:
+      "Subjective graph of observations, assumptions, questions, and interpretations",
+    tags: ["Observations", "Thoughts"],
+    image: "/images/apitest.png",
+    hasSource: false,
+  },
+];
 
-export const frontendTools = [
-    { name: "Vue", icon: "/placeholder.svg?height=64&width=64" },
-    { name: "React", icon: "/placeholder.svg?height=64&width=64" },
-    { name: "TypeScript", icon: "/placeholder.svg?height=64&width=64" },
-    { name: "JavaScript", icon: "/placeholder.svg?height=64&width=64" },
-    { name: "Nuxt", icon: "/placeholder.svg?height=64&width=64" },
-    { name: "Tailwind", icon: "/placeholder.svg?height=64&width=64" },
-    { name: "Git", icon: "/placeholder.svg?height=64&width=64" },
-    { name: "Shopify Liquid", icon: "/placeholder.svg?height=64&width=64" },
-    { name: "Formkit", icon: "/placeholder.svg?height=64&width=64" },
-    { name: "Node.js", icon: "/placeholder.svg?height=64&width=64" },
-    { name: "Redux Toolkit", icon: "/placeholder.svg?height=64&width=64" },
-    { name: "Apollo Client", icon: "/placeholder.svg?height=64&width=64" },
-    { name: "MUI", icon: "/placeholder.svg?height=64&width=64" },
-    { name: "Pinia", icon: "/placeholder.svg?height=64&width=64" },
-    { name: "Directus", icon: "/placeholder.svg?height=64&width=64" },
-    { name: "Bootstrap", icon: "/placeholder.svg?height=64&width=64" },
-    { name: "Chart.js", icon: "/placeholder.svg?height=64&width=64" },
-    { name: "HTML5", icon: "/placeholder.svg?height=64&width=64" },
-    { name: "CSS3", icon: "/placeholder.svg?height=64&width=64" },
-]
+export const Languages = [
+  { name: "Golang", icon: "/images/go.svg" },
+  { name: "TypeScript", icon: "/images/typescript.svg" },
+  { name: "JavaScript", icon: "/images/javascript.svg" },
+  { name: "HTML5", icon: "/images/html.svg" },
+  { name: "CSS3", icon: "/images/css.svg" },
+];
 
-export const backendTools = [
-    { name: "Node.js", icon: "/placeholder.svg?height=64&width=64" },
-    { name: "Express", icon: "/placeholder.svg?height=64&width=64" },
-    { name: "PostgreSQL", icon: "/placeholder.svg?height=64&width=64" },
-    { name: "MongoDB", icon: "/placeholder.svg?height=64&width=64" },
-    { name: "GraphQL", icon: "/placeholder.svg?height=64&width=64" },
-    { name: "Redis", icon: "/placeholder.svg?height=64&width=64" },
-    { name: "Docker", icon: "/placeholder.svg?height=64&width=64" },
-    { name: "Nginx", icon: "/placeholder.svg?height=64&width=64" },
-]
+export const Frameworks = [
+  { name: "React", icon: "/images/react.svg" },
+  { name: "NextJS", icon: "/images/nextjs.svg" },
+  { name: "NestJS", icon: "/images/nestjs.svg" },
+  { name: "Node.js", icon: "/images/nodejs.svg" },
+  { name: "Express", icon: "/images/express.svg" },
+  { name: "Redux Toolkit", icon: "/images/redux.svg" },
+  { name: "MUI", icon: "/images/mui.svg" },
+  { name: "Bootstrap", icon: "/images/bootstrap.svg" },
+  { name: "Tailwind", icon: "/images/tailwindcss.svg" },
+];
 
-export const cloudTools = [
-    { name: "AWS", icon: "/placeholder.svg?height=64&width=64" },
-    { name: "Google Cloud", icon: "/placeholder.svg?height=64&width=64" },
-    { name: "Azure", icon: "/placeholder.svg?height=64&width=64" },
-    { name: "Heroku", icon: "/placeholder.svg?height=64&width=64" },
-    { name: "DigitalOcean", icon: "/placeholder.svg?height=64&width=64" },
-    { name: "Cloudflare", icon: "/placeholder.svg?height=64&width=64" },
-    { name: "Firebase", icon: "/placeholder.svg?height=64&width=64" },
-    { name: "Netlify", icon: "/placeholder.svg?height=64&width=64" },
-]
+export const Databases = [
+  { name: "PostgreSQL", icon: "/images/postgresql.svg" },
+  { name: "MongoDB", icon: "/images/mongodb.svg" },
+  { name: "MySQL", icon: "/images/mysql.svg" },
+];
 
-export const deploymentTools = [
-    { name: "Docker", icon: "/placeholder.svg?height=64&width=64" },
-    { name: "Kubernetes", icon: "/placeholder.svg?height=64&width=64" },
-    { name: "Jenkins", icon: "/placeholder.svg?height=64&width=64" },
-    { name: "GitLab CI", icon: "/placeholder.svg?height=64&width=64" },
-    { name: "Travis CI", icon: "/placeholder.svg?height=64&width=64" },
-    { name: "CircleCI", icon: "/placeholder.svg?height=64&width=64" },
-    { name: "GitHub Actions", icon: "/placeholder.svg?height=64&width=64" },
-    { name: "ArgoCD", icon: "/placeholder.svg?height=64&width=64" },
-]
+export const Tools = [
+  { name: "AWS", icon: "/images/aws.svg" },
+  { name: "Google Cloud", icon: "/images/gcp.svg" },
+  { name: "Vercel", icon: "/images/vercel.svg" },
+  { name: "DigitalOcean", icon: "/images/docean.svg" },
+  { name: "Firebase", icon: "/images/firebase.svg" },
+  { name: "Netlify", icon: "/images/netlify.svg" },
+  { name: "Redis", icon: "/images/redis.svg" },
+  { name: "Nginx", icon: "/images/nginx.svg" },
+  { name: "Git", icon: "/images/git.svg" },
+  { name: "Docker", icon: "/images/docker.svg" },
+  { name: "Kubernetes", icon: "/images/kubernetes.svg" },
+  { name: "Jenkins", icon: "/images/jenkins.svg" },
+  { name: "GitLab CI", icon: "/images/gitlab.svg" },
+  { name: "CircleCI", icon: "/images/circleci.svg" },
+  { name: "GitHub Actions", icon: "/images/githubactions.svg" },
+];
 
 export const blogPosts: BlogPost[] = [
-    {
-        id: "system-design-sketches",
-        title: "System Design Sketches",
-        date: "Jul 18, 2024",
-        description: "System design sketches for popular system design interview questions.",
-        image: "/placeholder.svg?height=400&width=600",
-        socialLinks: {
-            facebook: "#",
-            twitter: "#",
-            youtube: "#",
-            whatsapp: "#",
-            uber: "#",
-            medium: "#"
-        }
+  {
+    id: "system-design-sketches",
+    title: "System Design Sketches",
+    date: "Jul 18, 2024",
+    description:
+      "System design sketches for popular system design interview questions.",
+    image: "/placeholder.svg?height=400&width=600",
+    socialLinks: {
+      facebook: "#",
+      twitter: "#",
+      youtube: "#",
+      whatsapp: "#",
+      uber: "#",
+      medium: "#",
     },
-    {
-        id: "micrograd-ts",
-        title: "Micrograd TS",
-        date: "Aug 7, 2023",
-        description: "A TypeScript version of karpathy/micrograd. A tiny scalar-valued autograd engine and a neural net on top of it.",
-        image: "/placeholder.svg?height=400&width=600"
-    },
-    {
-        id: "observations",
-        title: "Observations",
-        date: "Jun 27, 2023",
-        description: "My subjective observations, assumptions, questions, and interpretations about the world around us. Ultimate origin, science, Creator, theories, axioms, consciousness, universe, God's sensor, good vs evil, etc. Don't take it seriously.",
-        image: "/placeholder.svg?height=400&width=600"
-    }
-]
-
-
-
-
+  },
+  {
+    id: "micrograd-ts",
+    title: "Micrograd TS",
+    date: "Aug 7, 2023",
+    description:
+      "A TypeScript version of karpathy/micrograd. A tiny scalar-valued autograd engine and a neural net on top of it.",
+    image: "/placeholder.svg?height=400&width=600",
+  },
+  {
+    id: "observations",
+    title: "Observations",
+    date: "Jun 27, 2023",
+    description:
+      "My subjective observations, assumptions, questions, and interpretations about the world around us. Ultimate origin, science, Creator, theories, axioms, consciousness, universe, God's sensor, good vs evil, etc. Don't take it seriously.",
+    image: "/placeholder.svg?height=400&width=600",
+  },
+];
