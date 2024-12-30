25ad31da5accaa49fcefda0d8b8f96013235f748
Trend20 <enockomondi305@gmail.com>
2024-12-30 12:55:36 +0300

    code cleanup

--- lib/mdx-components.ts
+++
@@ -1,7 +0,0 @@
-import CodeBlock from "@/components/CodeBlock";
-import ImageWithCaption from "@/components/ImageWithCaption";
-
-export const mdxComponents = {
-  pre: CodeBlock,
-  ImageWithCaption,
-};
