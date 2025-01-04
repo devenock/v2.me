522672da3d90c317ec4176bed07c0d22eded8ab2
Trend20 <enockomondi305@gmail.com>
2025-01-04 23:00:12 +0300

    icon change

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <div className="flex flex-col md:flex-row gap-8 items-center md:items-start">
         <div className="w-48 h-48 sm:w-64 sm:h-64 relative rounded-full overflow-hidden shrink-0">
           <Image
-            src="/images/profile.png"
+            src="/images/icon.jpeg"
             alt="Oleksii Trekhleb"
             fill
             className="object-cover"
